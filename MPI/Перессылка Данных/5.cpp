edfaa1bdba3e1918ad306c7bf06200fd756a1fd8
Кирилл Ямщиков <yamshchikov_k@mail.ru>
2023-11-01 18:28:51 +0300

    Fix labs

--- MPI/Перессылка Данных/5.cpp
+++ MPI/Перессылка Данных/5.cpp
@@ -1,32 +1,99 @@
-// 5: В процессах чётного ранга ввести целое число, в процессах нечётного ранга ввести вещественное число. В каждом процессе вывести удвоенное значение введённого числа.
+/*
+* 5.
+* В процессах чётного ранга (включая главный) ввести целое число, в процессах нечётного ранга ввести вещественное число.
+* В каждом процессе вывести удвоенное значение введённого числа.
+*/
 
-#include <stdio.h>
 #include <mpi.h>
+#include <iostream>
+#include <vector>
 
-int main(int argc, char **argv)
-{
+using namespace std;
+
+int process_even_rank(int rank, int number) {
+    return number * 2;
+}
+
+double process_odd_rank(int rank, double number) {
+    return number * 2.0;
+}
+
+int main(int argc, char** argv) {
+    srand(time(NULL));
+
     MPI_Init(&argc, &argv);
 
+    // Получение ранга текущего процесса
     int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    double number;
-    if (rank % 2 == 0)
-    {
-        int int_number;
-        printf("Процесс %d: Введите целое число: ", rank);
-        scanf_s("%d", &int_number);
-        number = 2 * int_number;
-    }
-    else
-    {
-        printf("Процесс %d: Введите вещественное число: ", rank);
-        scanf_s("%lf", &number);
-        number *= 2;
-    }
+    // Получение размера коммуникатора MPI_COMM_WORLD
+    int size;
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    printf("Процесс %d: Удвоенное значение = %lf\n", rank, number);
+    // Переменные для хранения результатов
+    int result_int;
+    double result_double;
+
+    if (rank == 0) {
+        // Процесс с рангом 0 читает свой собственный ввод и выполняет свои операции
+        int number;
+        cout << "Enter an integer for the process with rank " << rank << ": ";
+        cin >> number;
+        result_int = process_even_rank(rank, number);
+
+        // Затем он читает ввод для других процессов и отправляет его им
+        for (int i = 1; i < size; ++i) {
+            if (i % 2 == 0) {
+                cout << "Enter an integer for the process with rank " << i << ": ";
+                cin >> number;
+                MPI_Send(&number, 1, MPI_INT, i, 0, MPI_COMM_WORLD);
+            }
+            else {
+                double real_number;
+                cout << "Enter a real number for the process with rank " << i << ": ";
+                cin >> real_number;
+                MPI_Send(&real_number, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+            }
+        }
+
+        // Выводит свои собственные результаты
+        cout << "Process with rank " << rank << " received the number " << result_int / 2 << "\n";
+        cout << "Doubled value: " << result_int << "\n";
+
+        // Запрашивает и выводит результаты от других процессов
+        for (int i = 1; i < size; ++i) {
+            if (i % 2 == 0) {
+                MPI_Recv(&result_int, 1, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                cout << "Process with rank " << i << " received the number " << result_int / 2 << "\n";
+                cout << "Doubled value: " << result_int << "\n";
+            }
+            else {
+                MPI_Recv(&result_double, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                cout << "Process with rank " << i << " received the number " << result_double / 2.0 << "\n";
+                cout << "Doubled value: " << result_double << "\n";
+            }
+        }
+    }
+    else {
+        // Каждый процесс получает свой ввод от процесса с рангом 0 и выполняет свои операции
+        if (rank % 2 == 0) {
+            int number;
+            MPI_Recv(&number, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            result_int = process_even_rank(rank, number);
+            // Отправляет результаты обратно в процесс с рангом 0
+            MPI_Send(&result_int, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        }
+        else {
+            double number;
+            MPI_Recv(&number, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            result_double = process_odd_rank(rank, number);
+            // Отправляет результаты обратно в процесс с рангом 0
+            MPI_Send(&result_double, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+        }
+    }
 
     MPI_Finalize();
+
     return 0;
 }
--- MPI/Процессы и Ранги/2.cpp
+++ MPI/Процессы и Ранги/2.cpp
@@ -1,32 +1,68 @@
-// 2: Пересылка вещественных чисел в подчинённые процессы
+/*
+* 2.
+* В главном процессе дан набор вещественных чисел;
+* количество чисел равно количеству подчинённых процессов.
+* С помощью функции MPI_Send переслать по одному числу в каждый из подчинённых процессов
+* (первое число в процесс 1, второе — в процесс 2, и т. д.) и вывести в подчинённых процессах полученные числа.
+*/
 
-#include <stdio.h>
 #include <mpi.h>
+#include <vector>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
-int main(int argc, char **argv)
-{
+using namespace std;
+
+int main(int argc, char** argv) {
+
     MPI_Init(&argc, &argv);
 
-    int rank, size;
+    int rank;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+    int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
+    vector<double> numbers(size);
     double number;
-    if (rank == 0)
-    {
-        printf("Введите вещественное число: ");
-        scanf("%lf", &number);
-        for (int i = 1; i < size; i++)
-        {
-            MPI_Send(&number, 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+
+    srand(time(NULL) + rank);  // Инициализация генератора случайных чисел
+
+    if (rank == 0) {
+        for (int i = 0; i < size; ++i) {
+            numbers[i] = (rand() % 101) * 1.0;  // Заполнение случайными числами от 0 до 100
         }
+
+        for (int i = 1; i < size; ++i) {
+            MPI_Send(&numbers[i], 1, MPI_DOUBLE, i, 0, MPI_COMM_WORLD);
+        }
+
+        number = numbers[0];
     }
-    else
-    {
+    else {
         MPI_Recv(&number, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        printf("Процесс %d: Полученное вещественное число = %lf\n", rank, number);
+    }
+
+    vector<double> all_numbers(size);
+    MPI_Gather(&number, 1, MPI_DOUBLE, all_numbers.data(), 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    if (rank == 0) {
+        cout << "Все числа из массива, начиная с нуля:" << endl;
+        for (int i = 1; i < size; ++i) {
+            cout << numbers[i] << "\t";
+        }
+        cout << endl;
+
+        for (int i = 1; i < size; ++i) {
+            cout << "Process with rank " << i << " recieved number: " << all_numbers[i] << endl;
+        }
+
+        cout << endl;
     }
 
     MPI_Finalize();
+
     return 0;
 }
